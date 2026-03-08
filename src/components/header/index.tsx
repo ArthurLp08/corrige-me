5291f14d9f8bee73bb478a00fd017f7e763cb9ef
ArthurLp08 <josearthurlda@gmail.com>
2026-03-08 20:31:23 -0300

    Formulario de redações feito

---
+++ src/components/header/index.tsx
@@ -0,0 +1,51 @@
+
+import styles from './styles.module.css';
+import { FaBars } from "react-icons/fa"
+import { IoCloseSharp } from "react-icons/io5";
+import { useState } from 'react';
+
+export default function Header() {
+
+    const [sanfona, setSanfona] = useState(false);
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.container}>
+
+
+                <a href='/' className={styles.headerTitle}>Corrige-Me*</a>
+                <div className={styles.sanfona}>
+
+                    <button onClick={() => { setSanfona(!sanfona) }} className={styles.sanfonaBotão}>{sanfona ? (
+                        <IoCloseSharp size={24} />
+                    ) : (
+
+                        <FaBars size={20} />
+                    )
+
+                    }</button>
+
+                    {sanfona ? (
+                        <div className={styles.sanfonaContainer}>
+                        <div className={styles.sanfonaConteudo}>
+                            <a href="/">
+                                Painel
+                            </a>
+                            <a href="/corrigir">
+                                Escrever
+                            </a>
+                            <a href="/historico">
+                                Minhas Redações
+                            </a>
+                            <button>Sair</button>
+                        </div>
+                        </div>
+                    ) : (
+                        <></>
+                    )}
+
+                </div>
+            </div>
+        </header>
+    )
+}
