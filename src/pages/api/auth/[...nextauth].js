58bc705da82f6deb3f8906e08853567c5a010524
ArthurLp08 <josearthurlda@gmail.com>
2026-03-10 21:20:35 -0300

    Bug fix da autenticação

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import Google from "next-auth/providers/google"
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
+    Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
